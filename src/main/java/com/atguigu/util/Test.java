bb234ca17442a2de10237b1f8fce8562798bc606
刘振兴 <1913054613@qq.com>
2021-02-02 19:58:24 +0800

    修改后的代码

--- src/main/java/com/atguigu/util/Test.java
+++ src/main/java/com/atguigu/util/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("我真的是太难了");
         System.out.println("Good Good Study! day day up");
+        System.out.println("凑个数把");
     }
 }
