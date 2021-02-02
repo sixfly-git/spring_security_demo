e64d72256c78e0cc0c1730a4646b5b88b23ea0a2
sixfly-git <78328499+sixfly-git@users.noreply.github.com>
2021-02-02 19:33:23 +0800

    Update Test.java

--- src/main/java/com/atguigu/util/Test.java
+++ src/main/java/com/atguigu/util/Test.java
@@ -3,6 +3,6 @@ package com.atguigu.util;
 public class Test {
     public static void main(String[] args) {
         System.out.println("我真的是太难了");
-        System.out.println("Good Good Study!");
+        System.out.println("Good Good Study! day day up");
     }
 }
