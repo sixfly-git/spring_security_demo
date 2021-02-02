c6f86b14e3ae63a018a510e8a9b1f274d644f4b9
刘振兴 <1913054613@qq.com>
2021-02-02 19:10:23 +0800

    idea测试HelloWorld之DEV分支

---
+++ src/main/java/com/atguigu/util/Test.java
@@ -0,0 +1,8 @@
+package com.atguigu.util;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("我真的是太难了");
+        System.out.println("Good Good Study!");
+    }
+}
